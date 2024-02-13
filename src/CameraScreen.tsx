8c59aac5b5d09dc64d80949ba049e15846484928
serapkececi <serapkececi@aof.anadolu.edu.tr>
2024-02-14 00:47:57 +0300

    v1

--- src/CameraScreen.tsx
+++ src/CameraScreen.tsx
@@ -1,37 +1,62 @@
-import React, {useState, useRef} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {RNCamera} from 'react-native-camera';
-import {useNavigation} from '@react-navigation/native';
+import React, { useState, useRef } from 'react';
+import { View, Text, TouchableOpacity, Alert, Image, Button } from "react-native";
+import { RNCamera } from 'react-native-camera';
+import { useNavigation } from "@react-navigation/native";
 
 const CameraScreen = () => {
   const navigation = useNavigation();
+  const camAlert = () => {
+    Alert.alert(
+      "Dikkat",
+      "Fotoğraf gönderiliyor",
+      [
+        {
+          text: "fotoğrafı gönder",
+          onPress: () => navigation.navigate("Home", {photoUri})
+        },
+        {
+          text: "yeni fotoğraf çek",
+          onPress: () => console.log("hello")
+        }
+      ],
+      { cancelable: false }
+    );
+  };
+
   const cameraRef = useRef(null);
   const [photoUri, setPhotoUri] = useState(null);
 
   const takePicture = async () => {
     if (cameraRef.current) {
-      const options = {quality: 0.5, base64: true };
+      const options = { quality: 0.5, base64: true };
       const data = await cameraRef.current.takePictureAsync(options);
       setPhotoUri(data.uri);
     }
   };
-  export const takePhotoNavigate = () => {
-    takePicture();
-    navigation.navigate('Home', { uri: photoUri });
-  };
+
   return (
     <View style={{flex: 1}}>
-      <RNCamera
-        ref={cameraRef}
-        style={{flex: 1 }}
-        type={RNCamera.Constants.Type.back}
-        captureAudio={false}>
-        <View style={{ flex: 1, justifyContent: "flex-end", alignItems: "center" }}>
-          <TouchableOpacity onPress={takePhotoNavigate} style={{ marginBottom: 20 }}>
-            <Text style={{ fontSize: 20, marginBottom: 13, color: "white" }}>Fotoğraf Çek</Text>
-          </TouchableOpacity>
+      {photoUri ? (
+        <View>
+          <Text>Fotoğraf çekildi!</Text>
+          <Text>{photoUri}</Text>
+          <Image style={{width:100, height:100}} source={{uri:photoUri}}/>
+          <Button title={'upload'} onPress={camAlert} />
         </View>
-      </RNCamera>
+      ) : (
+        <RNCamera
+          ref={cameraRef}
+          style={{ flex: 1 }}
+          type={RNCamera.Constants.Type.back}
+          captureAudio={false}
+        >
+          <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+            <TouchableOpacity onPress={takePicture} style={{ marginBottom: 20 }}>
+              <Text style={{ fontSize: 20, marginBottom: 13, color: 'white' }}>Fotoğraf Çek</Text>
+            </TouchableOpacity>
+          </View>
+        </RNCamera>
+      )}
     </View>
   );
 };
--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,40 +1,17 @@
 import React from "react";
 import { Alert, Button, Image, Text, View } from "react-native";
-import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
-
-interface RouteProps {
-  uri: string;
-}
 export default function HomeScreen() {
-  const {params } = useRoute<RouteProp<RouteProps>>();
   const navigation = useNavigation();
-  const camAlert = () => {
-    Alert.alert(
-      'Dikkat',
-      'Fotoğraf gönderiliyor',
-      [
-        {
-          text: 'fotoğrafı gönder',
-          onPress: () => navigation.navigate('First'),
-        },
-        {
-          text: 'yeni fotoğraf çek',
-          onPress: () => console.log('hello'),
-        },
-      ],
-      {cancelable: false},
-    );
-  };
-
+  const route = useRoute();
+  const {photoUri} = route.params || {};
+  console.log("ccccc", photoUri)
   return (
     <View>
       <View>
-        <Text>Fotoğraf çekildi!</Text>
-        <Text>çekilen fotoğraf</Text>
-        <Image style={{ width: 100, height: 100 }} source={{ uri: params.uri }} />
+        <Image style={{ width: 100, height: 100 }} source={{ uri: photoUri}} />
       </View>
-      <Button title={"tıkla"} onPress={camAlert} />
     </View>
   );
 }
